66f743703911b84079f51b84a426b81acbb81056
vjohn <perljohn@gmail.com>
2024-06-17 14:26:17 +0530

    jenkins

---
+++ create_file_tmp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+                echo "####===     vim  /tmp/Jenkins_running.txt     =============================##### "
+                          vim  /tmp/Jenkins_running.txt 
+                echo "  ls  >> /tmp/Jenkins_running.txt    ############################# "
+                       ls  >> /tmp/Jenkins_running.txt   
+                echo "####=================================================================================##### "
+                      ls -l /tmp/Jenkins_running.txt 
+                    echo -e " Jenkins working     🍇    🍈 🍉  🍊 🍋  🍍 🥭  🍎  🍏 🍐 🍑   /n/n "
+                fi
+
+
